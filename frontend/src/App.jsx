e85b113bb468aeee393ba204e190f2a0c8df65ef
ArcaneCipher <desjardins.jarp@gmail.com>
2025-02-17 23:27:32 -0500

    Refactor PhotoListItem to accept props

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,36 @@
-import React from 'react';
+import React from "react";
+import PhotoListItem from "./components/PhotoListItem";
+import "./App.scss";
 
-import PhotoListItem from './components/PhotoListItem';
-import './App.scss';
+const sampleDataForPhotoListItem = {
+  id: 1,
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  urls: {
+    full: "/Image-1-Full.jpeg",
+    regular: "/Image-1-Regular.jpeg",
+  },
+  user: {
+    username: "exampleuser",
+    name: "Joe Example",
+    profile: "/profile-1.jpg",
+  },
+};
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem />
+      <PhotoListItem
+        imageSource={sampleDataForPhotoListItem.urls.regular}
+        username={sampleDataForPhotoListItem.user.name}
+        profile={sampleDataForPhotoListItem.user.profile}
+        location={sampleDataForPhotoListItem.location}
+      />
     </div>
   );
 };
 
 export default App;
-
-
-/* 
-import React from 'react';
-
-import PhotoListItem from './components/PhotoListItem';
-import './App.scss';
-
-// Note: Rendering a single component to build components in isolation
-const App = () => (
-  <div className="App">
-    <PhotoListItem/>
-  </div>
-)
-
-export default App
-*/
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,59 +1,19 @@
 import React, { useState } from 'react';
 import "../styles/PhotoListItem.scss";
 
-const sampleDataForPhotoListItem = {
-  id: 1,
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  urls: {
-    full: "/Image-1-Full.jpeg",
-    regular: "/Image-1-Regular.jpeg",
-  },
-  user: {
-    username: "exampleuser",
-    name: "Joe Example",
-    profile: "/profile-1.jpg",
-  },
-};
-
-const PhotoListItem = () => {
+const PhotoListItem = ({ imageSource, username, profile, location }) => {
   return (
     <div className="photo-list-item">
-      <img src={sampleDataForPhotoListItem.urls.regular} alt="Photo" className="photo" />
+      <img src={imageSource} alt="Photo" className="photo" />
       <div className="photo-details">
-        <img src={sampleDataForPhotoListItem.user.profile} alt="Photographer" className="profile-img" />
+        <img src={profile} alt="Photographer" className="profile-img" />
         <div className="photographer-info">
-          <p className="photographer-name">{sampleDataForPhotoListItem.user.name}</p>
-          <p className="photographer-location">{sampleDataForPhotoListItem.location.city} {sampleDataForPhotoListItem.location.country}</p>
+          <p className="photographer-name">{username}</p>
+          <p className="photographer-location">{location.city}, {location.country}</p>
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default PhotoListItem;
-
-
-/* 
-import React from 'react';
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-
-const PhotoListItem = () => {
-  // Component Code
-}
-
-export default PhotoListItem
-*/
