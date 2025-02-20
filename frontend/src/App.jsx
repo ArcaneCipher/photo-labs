fc335e073dcd2cf682c69b1b70061278fa0ebb77
ArcaneCipher <desjardins.jarp@gmail.com>
2025-02-19 23:28:01 -0500

    Add TopNavigationBar component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.scss";
 import PhotoList from "./components/PhotoList";
-import TopicList from "./components/TopicList";
+import TopNavigation from "./components/TopNavigationBar";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
       <header>
-        <TopicList />
+        <TopNavigation />
       </header>
       <PhotoList />
     </div>
